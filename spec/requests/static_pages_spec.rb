f2a57e835e0bcd4087e5f8110efc0b80530941e5
ddancziger <ddancziger@gmail.com>
2012-12-10 04:14:48 -0300

    Static Pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,7 +5,48 @@ describe "Static pages" do
     describe "Home Page" do
         it "should have the content 'Sample App'" do
             visit '/static_pages/home'
-            page.should have_content('Sample App')
+            page.should have_selector('h1', :text => 'Sample App')
+        end
+
+        it 'should have the right title' do
+            visit '/static_pages/home'
+            page.should have_selector('title', :text => "Ruby on Rails Tutoria Sample App | Home")
+        end
+    end
+
+    describe "Help Page" do
+        it "should have the content 'Help'" do
+            visit '/static_pages/help'
+            page.should have_selector('h1', :text => 'Help')
+        end
+
+        it 'should have the right title' do
+            visit '/static_pages/help'
+            page.should have_selector('title', :text => "Ruby on Rails Tutoria Sample App | Help")
+        end
+    end
+
+    describe "Help Page" do
+        it "should have the content 'About Us'" do
+            visit '/static_pages/about'
+            page.should have_selector('h1', :text => 'About Us')
+        end
+
+        it 'should have the right title' do
+            visit '/static_pages/about'
+            page.should have_selector('title', :text => "Ruby on Rails Tutoria Sample App | About")
+        end
+    end
+
+    describe "Contact Page" do
+        it "should have the h1 'Contact'" do
+            visit '/static_pages/contact'
+            page.should have_selector('h1', :text => 'Contact')
+        end
+
+        it 'should have the right title' do
+            visit '/static_pages/contact'
+            page.should have_selector('title', :text => "Ruby on Rails Tutoria Sample App | Contact")
         end
     end
 end
