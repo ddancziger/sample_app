bebe4c26b9144e9932c31914167aeb7c89bff95e
ddancziger <ddancziger@gmail.com>
2012-12-11 04:45:38 -0300

    chapt 4

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+    #Return the full title on a per-pae basis
+    def full_title(page_title)
+        base_title = 'Ruby on Rails Tutoria Sample App '
+        if page_title.empty?
+            base_title
+        else
+            "#{base_title} | #{page_title}"
+        end
+    end
+
 end
+
